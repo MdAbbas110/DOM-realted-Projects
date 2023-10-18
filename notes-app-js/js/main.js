d79ccf144bb96969290e3410abe183ff2ddaa462
Abbas <meerabbas110@gmail.com>
2023-10-18 10:48:06 +0530

    note delete method with on dblclick event

--- notes-app-js/js/main.js
+++ notes-app-js/js/main.js
@@ -7,6 +7,15 @@ const view = new NotesView(app, {
   onNoteAdd() {
     // console.log('notes added');
   },
+
+  onNoteSelect(id) {
+    console.log('Notes selected' + id);
+  },
+
+  onNoteDelete(id) {
+    console.log('note Deleted: ' + id);
+  },
+
   onNoteEdit(newTitle, newBody) {
     console.log(newTitle);
     console.log(newBody);
