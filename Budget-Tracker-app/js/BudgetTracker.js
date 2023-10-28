eb3cc195fb2f8c1cecb5d171799be951d36b568b
Abbas <meerabbas110@gmail.com>
2023-10-28 21:06:13 +0530

    updating the dom via javascript

--- Budget-Tracker-app/js/BudgetTracker.js
+++ Budget-Tracker-app/js/BudgetTracker.js
@@ -11,20 +11,98 @@ export default class BudgetTracker {
     this.load();
   }
 
-  static html() {}
-  static entryHtml() {}
+  static html() {
+    return `
+    <table class="budget-tracker">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Type</th>
+            <th>Amount</th>
+            <th></th>
+          </tr>
+        </thead>
 
-  load() {}
+        <tbody class="entries">
+          
+        </tbody>
+
+        <tbody>
+          <tr>
+            <td class="controls" colspan="5">
+              <button type="button" class="new-entry">New Entry</button>
+            </td>
+          </tr>
+        </tbody>
+
+        <tfoot>
+          <tr>
+            <td class="summary" colspan="5">
+              <strong>Total</strong>
+              <span class="total">$0.00</span>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    `;
+  }
+
+  static entryHtml() {
+    return `
+    <tr>
+      <td>
+        <input type="date" class="input input-date" />
+      </td>
+      <td>
+        <input
+          type="text"
+          class="input input-description"
+          placeholder="Add a Description"
+        />
+      </td>
+      <td>
+        <select class="input input-type">
+          <option value="Income">Income</option>
+          <option value="Expense">Expense</option>
+        </select>
+      </td>
+      <td>
+        <input type="number" class="input input-amount" />
+      </td>
+      <td>
+        <button class="delete-entry" type="button">&#10005;</button>
+      </td>
+    </tr>
+    `;
+  }
+
+  load() {
+    const entries = JSON.parse(
+      localStorage.getItem('budget-tracker-entries-dev') || '[]'
+    );
+    console.log(entries);
+    for (const entry of entries) {
+      this.addEntry(entry);
+    }
+    this.updateSummary();
+  }
 
   updateSummary() {}
 
   save() {}
 
-  addEntry(entry = {}) {}
+  addEntry(entry = {}) {
+    this.root
+      .querySelector('.entries')
+      .insertAdjacentHTML('beforeend', BudgetTracker.entryHtml());
+  }
 
   getEntryRows() {}
 
-  onNewEntryBtnClick() {}
+  onNewEntryBtnClick() {
+    this.addEntry();
+  }
 
   onDeleteEntryBtnClick() {}
 }
